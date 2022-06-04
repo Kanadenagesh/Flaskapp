11c4ceeb63215362612e2b5614a247fbdc4db779
Nagesh Kanade <kanadens2019@gmail.com>
2022-06-04 15:27:27 -0400

    second commit

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def index():
   return("<b>This Flask application is Hosted by Nagesh kanade For testing Purpose!</b>")
 
+@app.route('/about')
+def about():
+  return("Hello i am Nagesh Kanade Enthusiast python Web developer!")
+
 if __name__ == '__main__':
 
  app.run()
